fea2311e80911e014ec1367831ccea8db677ead8
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2024-01-01 22:10:13 -0500

    taunting

--- Assets/ObjectInView.cs
+++ Assets/ObjectInView.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class GhostInViewChecker : MonoBehaviour
 {
     public Camera ovrCamera; // Assign your OVR Camera here
-    public AudioClip ghostSound; // Assign the audio clip for the ghost here
+    public AudioClip lookingAudio; // Assign the looking audio clip here
+    public AudioClip[] tauntingAudios; // Assign the array of taunting audio clips here
     private AudioSource audioSource;
+    private Coroutine tauntingCoroutine;
+    private bool isLookingAtGhost = false;
 
     void Start()
     {
-        // Get the AudioSource component
         audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
         GameObject[] ghosts = GameObject.FindGameObjectsWithTag("Ghost");
+        bool foundGhostInView = false;
 
         foreach (GameObject ghost in ghosts)
         {
@@ -25,11 +28,51 @@ public class GhostInViewChecker : MonoBehaviour
 
             if (angle < (ovrCamera.fieldOfView / 2))
             {
-                if (!audioSource.isPlaying)
+                foundGhostInView = true;
+                if (!isLookingAtGhost)
                 {
-                    Debug.Log("Ghost in view: " + ghost.name);
-                    audioSource.PlayOneShot(ghostSound);
+                    isLookingAtGhost = true;
+                    PlayLookingAudio();
                 }
+                break; // Found a ghost in view, no need to check others
+            }
+        }
+
+        if (!foundGhostInView && isLookingAtGhost)
+        {
+            isLookingAtGhost = false;
+            StartTauntingAudio();
+        }
+    }
+
+    void PlayLookingAudio()
+    {
+        if (tauntingCoroutine != null)
+        {
+            StopCoroutine(tauntingCoroutine);
+            tauntingCoroutine = null;
+        }
+        audioSource.clip = lookingAudio;
+        audioSource.Play();
+    }
+
+    void StartTauntingAudio()
+    {
+        if (tauntingCoroutine == null)
+        {
+            tauntingCoroutine = StartCoroutine(TauntingAudioCoroutine());
+        }
+    }
+
+    IEnumerator TauntingAudioCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(10, 21));
+            if (!isLookingAtGhost)
+            {
+                audioSource.clip = tauntingAudios[Random.Range(0, tauntingAudios.Length)];
+                audioSource.Play();
             }
         }
     }
